9844ce17f3ccae3b05307cbeee9be2dd0b4d78aa
Khaled Badran <gym4programming@gmail.com>
2022-10-18 01:41:09 +0200

    WIP: basics of darkMode

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
+import {createContext} from 'react'
+
+export const darkModeContext = createContext()
+
 function App() {
+// dark mode home page #181818
 	return (
 		<div className="container mx-auto md:px-20">
-			<Navbar />
-			<Home />
-			<Projects />
-			<About />
-			<Contact />
-			<Footer />
+			<darkModeContext.Provider value={true}>
+				<Navbar />
+				<Home />
+				<Projects />
+				<About />
+				<Contact />
+				<Footer />
+			</darkModeContext.Provider>
 		</div>
 	);
 }
