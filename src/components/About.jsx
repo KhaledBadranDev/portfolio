ed8483bb91c41694f62e2bef9377253f20b8252e
Khaled Badran <gym4programming@gmail.com>
2022-10-17 22:58:53 +0200

    adding header and margin

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,33 @@
+import AnimatedLetters from './AnimatedLetters'
+import { useEffect, useState } from 'react'
 
 const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const sectionHeader = ["A", "b", "o", "u", "t", " ", "M", "e"]
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 2000)
+    }, [])
+
     return(
-        <p className="m-12" id="about">
+        <section className="mt-40 mb-40" id="about">
+            <header>
+                <h1 className="mb-10 text-5xl font-bold flex place-content-center">
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={sectionHeader}
+                    />
+                </h1>
+            </header>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Perferendis veniam, nesciunt eos voluptate adipisci ullam.
             Ad necessitatibus vero cumque suscipit iste, minus veritatis 
             tenetur vel illum natus doloremque quisquam commodi maxime eaque dicta
             sapiente corporis? Beatae quasi nobis quis id. Nesciunt dolores 
 
-        </p>
+        </section>
     );
 }
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,28 @@
+import AnimatedLetters from './AnimatedLetters'
+import { useEffect, useState } from 'react'
+
 
 const Contact = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const sectionHeader = ["C", "o", "n", "t", "a", "c", "t", " ", "M", "e"]
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 2000)
+    }, [])
+
+
     return(
-        <section className="m-12" id="contact">
+        <section className="mt-40 mb-40" id="contact">
+            <header>
+                <h1 className="mb-10 text-5xl font-bold flex place-content-center">
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={sectionHeader}
+                    />
+                </h1>
+            </header>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Perferendis veniam, nesciunt eos voluptate adipisci ullam.
             Ad necessitatibus vero cumque suscipit iste, minus veritatis 
