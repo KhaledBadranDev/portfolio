29d3cc6d5342807100a0b1edeaa34e8b565ec60b
Khaled Badran <gym4programming@gmail.com>
2022-10-19 07:46:27 +0200

    fixed: glitch while changing darkMode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     const [darkMode, setDarkMode] = useState(useContext(darkModeContext));
     return (
         <div className={darkMode ? "dark" : ""}>
-            <nav className="z-20 shadow-xl w-full fixed top-0 left-0 bg-white dark:bg-[#212121]">
+            <nav className="z-20 shadow-xl w-full fixed top-0 left-0 transition-all duration-500 ease-in bg-white dark:bg-[#212121]">
                 <div className='container mx-auto px-20 sm:px-7 py-2 sm:flex items-center justify-between'>
                     <span className='font-bold text-2xl flex items-center font-[Poppins] text-gray-800'>
                         <AnchorLink href="#home">
@@ -21,7 +21,7 @@ const Navbar = () => {
                     <div onClick={() => setOpen(!open)} className='text-3xl absolute right-4 top-1 cursor-pointer sm:hidden'>
                         {open ? <ImMenu4 style={{ fontSize: '40px' }} /> : <ImMenu3 style={{ fontSize: '40px' }} />}
                     </div>
-                    {/* //TODO: fix glitch while changing darkMode */}
+
                     <ul className={`sm:flex grid justify-items-end sm:items-center sm:pb-0 pb-6 absolute sm:static dark:bg-[#212121] bg-white shadow-xl sm:shadow-none sm:z-auto z-[-1] left-0 w-full sm:w-auto sm:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-0' : 'top-[-490px]'}`}>
                         <li className="sm:ml-8 mr-4 text-xl sm:my-0 mt-16">
                             <AnchorLink href="#projects" className='hover:text-indigo-700 duration-300'>
