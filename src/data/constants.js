bfec544540cef3bfbc31cb575711106d5c18f0bb
Khaled Badran <gym4programming@gmail.com>
2022-12-27 23:06:01 -0700

    testing long tech stack array

--- src/data/constants.js
+++ src/data/constants.js
@@ -4,7 +4,11 @@ const embeddedGames = {
     "title": "Embedded Games",
     "techStack": [
         "React.js",
-        "Tailwindcss",
+        "Bootstrap",
+        "React.js",
+        "Bootstrap",
+        "React.js",
+        "Bootstrap",
     ],
     "briefDescription": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est assumenda quia minus, in, quos aperiam mollitia perspiciatis veniam veritatis deleniti blanditiis cum? Maxime iusto nam quo autem at excepturi cum voluptatum accusantium cumque alias molestias laudantium, non similique ipsam adipisci aut sapiente doloremque maiores nobis nulla? Quos, aut. Dolorum suscipit quisquam pariatur consectetur accusamus dolores voluptas, doloribus veniam exercitationem blanditiis eveniet quibusdam iusto illo autem, enim provident tempora excepturi sit? Expedita, harum, error est quam nesciunt perferendis illo eius amet velit, enim aliquid incidunt nostrum ea molestiae? Animi, unde expedita minus at cum distinctio libero quaerat maiores saepe praesentium nam?"
 }
@@ -14,7 +18,7 @@ const thisPortfolio = {
     "title": "This Portfolio",
     "techStack": [
         "React.js",
-        "Bootstrap",
+        "Tailwindcss",
     ],
     "briefDescription": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est assumenda quia minus, in, quos aperiam mollitia perspiciatis veniam veritatis deleniti blanditiis cum? Maxime iusto nam quo autem at excepturi cum voluptatum accusantium cumque alias molestias laudantium, non similique ipsam adipisci aut sapiente doloremque maiores nobis nulla? Quos, aut. Dolorum suscipit quisquam pariatur consectetur accusamus dolores voluptas, doloribus veniam exercitationem blanditiis eveniet quibusdam iusto illo autem, enim provident tempora excepturi sit? Expedita, harum, error est quam nesciunt perferendis illo eius amet velit, enim aliquid incidunt nostrum ea molestiae? Animi, unde expedita minus at cum distinctio libero quaerat maiores saepe praesentium nam?"
 }
