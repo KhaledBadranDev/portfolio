503963ee65ccd38b1e934b90abf700990fdc4b35
Khaled Badran <gym4programming@gmail.com>
2022-12-29 21:14:55 -0700

    adding animated down arrow

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
     }, [])
 
     return (
-        <section className="container mx-auto pt-40 pb-40" id="home">
+        <section className="container mx-auto pt-40" id="home">
             <header className="mb-10 flex place-content-center 2xl:text-7xl xl:text-6xl lg:text-5xl md:text-4xl sm:text-3xl text-base sm:font-sans font-mono font-bold">
                 <h1 className=" text-teal-500">
                     <div className = "flex place-content-center">
@@ -42,8 +42,8 @@ const Home = () => {
                     />
                 </h1>
             </header>
-            <div className="flex place-content-center sm:mt-20 mt-5">
-                <AnchorLink offset='150' href="#about" className='hover:text-violet-700 text-teal-500 duration-300'>
+            <div className="flex place-content-center sm:mt-40 mt-5">
+                <AnchorLink offset='150' href="#about" className='hover:text-violet-700 text-teal-500'>
                     <Button type="Read" text="More About Me"/>
                 </AnchorLink>
             </div>
@@ -58,6 +58,13 @@ const Home = () => {
                     <FaBlogger className="md:ml-12 sm:ml-8 ml-4 text-xl sm:my-10 my-5 hover:text-violet-700 text-teal-500 duration-300" style={{ fontSize: '25px' }} />
                 </a>
             </figure>
+            <div className="flex place-content-center mt-60">
+                <div className=" flex items-center justify-center animate-bounce bg-white dark:bg-[#181818] p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full">
+                    <svg className="w-12 h-12 text-violet-700" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                        <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+                    </svg>
+                </div>
+            </div>
         </section>
     )
 }
