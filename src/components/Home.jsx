739d2ca671a7c39192c18a2c219eaa38fbf92438
Khaled Badran <gym4programming@gmail.com>
2023-01-16 22:22:24 -0700

    improve offset anchor link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import AnimatedLetters from './AnimatedLetters'
 import Button from "./Button"
 import AnchorLink from 'react-anchor-link-smooth-scroll'
@@ -10,7 +10,7 @@ const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const hiArr = ['H', 'i', ',']
     const nameArr = ['I', ' ', 'a', 'm', ' ', 'K', 'h', 'a', 'l', 'e', 'd', ' ', 'B', 'a', 'd', 'r', 'a', 'n', "👋"]
-    const jobArr = ['F','u','l','l', ' ','S','t','a','c','k', ' ', 'S', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+    const jobArr = ['F', 'u', 'l', 'l', ' ', 'S', 't', 'a', 'c', 'k', ' ', 'S', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
 
     useEffect(() => {
         setTimeout(() => {
@@ -22,7 +22,7 @@ const Home = () => {
         <section className="container mx-auto pt-40" id="home">
             <header className="mb-10 flex place-content-center 2xl:text-7xl xl:text-6xl lg:text-5xl md:text-4xl sm:text-3xl text-base sm:font-sans font-mono font-bold">
                 <h1 className=" text-teal-500">
-                    <div className = "flex place-content-center">
+                    <div className="flex place-content-center">
                         <AnimatedLetters
                             letterClass={letterClass}
                             strArray={hiArr}
@@ -44,7 +44,7 @@ const Home = () => {
             </header>
             <div className="flex place-content-center sm:mt-40 mt-5">
                 <AnchorLink offset='150' href="#about" className='hover:text-violet-700 text-teal-500'>
-                    <Button type="Read" text="More About Me"/>
+                    <Button type="Read" text="More About Me" />
                 </AnchorLink>
             </div>
             <figure className="text-5xl flex justify-center py-3 ">
@@ -58,12 +58,15 @@ const Home = () => {
                     <FaBlogger className="md:ml-12 sm:ml-8 ml-4 text-xl sm:my-10 my-5 hover:text-violet-700 text-teal-500 duration-300" style={{ fontSize: '25px' }} />
                 </a>
             </figure>
-            <div className="flex place-content-center mt-60">
-                <div className=" flex items-center justify-center animate-bounce bg-white dark:bg-[#181818] p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full">
-                    <svg className="w-12 h-12 text-violet-700" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                        <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-                    </svg>
-                </div>
+            <div className="flex place-content-center mt-[70px] mb-60 pb-40">
+                <AnchorLink offset='150' href="#projects" className='hover:text-violet-700 text-teal-500'>
+                    <div className=" flex items-center justify-center animate-bounce bg-white dark:bg-[#181818] p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full">
+                        <svg className="w-12 h-12 text-violet-700" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                            <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+                        </svg>
+                    </div>
+                </AnchorLink>
+
             </div>
         </section>
     )
