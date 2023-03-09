a727f3a2b7748edc554a19a6c7b52685b7e8bfad
Khaled Badran <khaledb.dev@gmail.com>
2023-03-08 21:57:37 -0700

    faet: WIP the new projects sections buttons

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -71,6 +71,15 @@ const Projects = () => {
 
             </div>
             
+            {/* TODO buttons  */}
+    <div className='button w-40 h-16 bg-blue-500 rounded-lg cursor-pointer select-none
+    active:translate-y-2  active:[box-shadow:0_0px_0_0_#1b6ff8,0_0px_0_0_#1b70f841]
+    active:border-b-[0px]
+    transition-all duration-150 [box-shadow:0_10px_0_0_#1b6ff8,0_15px_0_0_#1b70f841]
+    border-b-[1px] border-blue-400
+  '>
+		<span className='flex flex-col justify-center items-center h-full text-white font-bold text-lg '>Active</span>
+	</div>
 
             <main className="2xl:mx-60 xl:mx-50 lg:mx-40 md:mx-20 sm:mx-10 mx-6">
                 {webProjectsArr.map(projectObj => (
